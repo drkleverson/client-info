e839de0597391f117131f30bf59431a12f4641ed
Kleverson Bona <kleverson@evoplan.com.br>
2021-08-20 09:34:12 -0300

    add device detection

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,14 +1,19 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
+import DeviceDetector from "device-detector-js";
 
 module.exports = async (req: VercelRequest, res: VercelResponse) => {
   const response = await fetch(
     `http://ip-api.com/json/${req.headers["x-real-ip"] || ""}`
   );
+
   const responseData = await response.json();
+  const deviceDevice = new DeviceDetector();
+  const device = deviceDevice.parse(req.headers["user-agent"]);
 
   const data = {
     ip: req.headers["x-real-ip"] || "",
     result: responseData,
+    device,
   };
 
   res.status(200).json(data);
