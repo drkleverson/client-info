255fe834dabf4223f55f625700526741b82432eb
Kleverson Bona <kleverson@evoplan.com.br>
2021-08-20 10:17:11 -0300

    remove cpu

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,6 +1,5 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
 import DeviceDetector from "device-detector-js";
-import si from "systeminformation";
 
 module.exports = async (req: VercelRequest, res: VercelResponse) => {
   const response = await fetch(
@@ -8,7 +7,6 @@ module.exports = async (req: VercelRequest, res: VercelResponse) => {
   );
 
   const responseData = await response.json();
-  const cpuInfo = await si.cpu();
   const deviceDevice = new DeviceDetector();
   const device = deviceDevice.parse(req.headers["user-agent"]);
 
@@ -16,7 +14,6 @@ module.exports = async (req: VercelRequest, res: VercelResponse) => {
     ip: req.headers["x-real-ip"] || "",
     result: responseData,
     device,
-    cpuInfo,
   };
 
   res.status(200).json(data);
