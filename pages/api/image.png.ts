c5939750ca3452c068074dda7e85d9b6ba38eac8
Kleverson Bona <kleverson@evoplan.com.br>
2021-09-17 11:35:27 -0300

    fix get ip spy pixel

--- pages/api/image.png.ts
+++ pages/api/image.png.ts
@@ -4,7 +4,11 @@ import { PNG } from "pngjs";
 import fs from "fs";
 
 module.exports = async (req: VercelRequest, res: VercelResponse) => {
-  console.log(req.headers["user-agent"], req.socket.remoteAddress);
+  console.log(
+    req.headers["user-agent"],
+    req.headers["x-real-ip"],
+    req.headers.forwarded
+  );
   const image = new PNG({ width: 1, height: 1 });
   res.setHeader("Content-Type", "image/png");
   res.status(200);
