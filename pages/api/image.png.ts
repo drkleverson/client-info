2dbccc6ff6341ad010c18ad76c41d09410cf39ea
Kleverson Bona <kleverson@evoplan.com.br>
2021-09-17 11:32:09 -0300

    add spy pixel

---
+++ pages/api/image.png.ts
@@ -0,0 +1,12 @@
+import { VercelRequest, VercelResponse } from "@vercel/node";
+import DeviceDetector from "device-detector-js";
+import { PNG } from "pngjs";
+import fs from "fs";
+
+module.exports = async (req: VercelRequest, res: VercelResponse) => {
+  console.log(req.headers["user-agent"], req.socket.remoteAddress);
+  const image = new PNG({ width: 1, height: 1 });
+  res.setHeader("Content-Type", "image/png");
+  res.status(200);
+  res.send(await image.pack());
+};
