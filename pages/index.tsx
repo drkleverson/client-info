757302c0a1791d47899ea08181b8512a3fe390b6
Kleverson Bona <kleverson@evoplan.com.br>
2022-03-29 09:26:49 -0300

    add style

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,20 @@ import DeviceDetector from "device-detector-js";
 import { GetServerSideProps } from "next";
 
 import { makeStyles } from "@material-ui/core/styles";
+import { Box, Card, Grid, Typography, CardContent } from "@material-ui/core";
+import { GiEarthAmerica } from "react-icons/gi";
+import { MdDevices } from "react-icons/md";
+import { FaNetworkWired, FaMapMarkerAlt } from "react-icons/fa";
+
 const useStyles = makeStyles((theme) => ({
   root: {
-    fontFamily:"Roboto"
+    fontFamily: "Roboto",
   },
+  icon: {
+    marginRight: 5,
+  }
 }));
+
 export default function Home({ device, result }) {
   const classes = useStyles();
   return (
@@ -15,24 +24,39 @@ export default function Home({ device, result }) {
       <Head>
         <title>Meu dispositivo</title>
       </Head>
-      <p>
-        ip: <b>{result.query}</b>
-      </p>
-      <p>provedor: {result.isp}</p>
-      <p>organização: {result.org}</p>
-      <p>sistema autônomo: {result.as}</p>
-      <p>
-        região: {result.city}, {result.regionName}, {result.country}
-      </p>
-      <p>
-        sistema operacional: {device.os.name} {device.os.version}
-      </p>
+      <Grid container justify="center">
+        <Card>
+          <CardContent>
+            <Box mb={1} alignItems="center" display="flex" flexWrap="wrap">
+              <GiEarthAmerica className={classes.icon} />
+              <Typography>
+                <b>{result.query}</b>
+              </Typography>
+            </Box>
+            <Box mb={1} alignItems="center" display="flex" flexWrap="wrap">
+              <FaNetworkWired className={classes.icon} />
+              <Typography>{result.isp}</Typography>
+            </Box>
+            <Box mb={1} alignItems="center" display="flex" flexWrap="wrap">
+              <FaMapMarkerAlt className={classes.icon} />
+              <Typography>
+                {result.city}, {result.regionName}, {result.country}
+              </Typography>
+            </Box>
+            <Box alignItems="center" display="flex" flexWrap="wrap">
+              <MdDevices className={classes.icon} />
+              <Typography>
+                {device.os.name} {device.os.version}
+              </Typography>
+            </Box>
+          </CardContent>
+        </Card>
+      </Grid>
     </div>
   );
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  
   const response = await fetch(
     `http://ip-api.com/json/${context.req.headers["x-real-ip"] || ""}`
   );
@@ -43,7 +67,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   return {
     props: {
       device,
-      result
+      result,
     },
   };
 };
